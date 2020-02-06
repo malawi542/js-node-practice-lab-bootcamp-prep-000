245aaa8ca6aaf84762d8482ecb4e24177b7997ee
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-02-06 23:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Muath"
-var height = "25"
+var height = 25
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
